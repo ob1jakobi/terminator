c2b5eb03c16b90bc18dc11861aca13c12bad9222
Jake McGrath <jake.k.mcgrath@gmail.com>
2023-10-29 17:58:59 -0500

    Made multiple modifications; might split into separate file(s).

--- terminator/src/bin/create_database.rs
+++ terminator/src/bin/create_database.rs
@@ -1,8 +1,9 @@
 use bcrypt::DEFAULT_COST;
+use chrono::{DateTime, Datelike, Utc};
 use rusqlite::Connection;
 use std::env;
 use std::io::{stdin, stdout, Write};
-use std::path::{Path, PathBuf};
+use std::path::{Path, PathBuf, MAIN_SEPARATOR_STR};
 
 const ASSET_DIR_NAME: &str = "assets";
 const DATABASE_NAME: &str = "terminator.db";
@@ -18,10 +19,8 @@ fn create_database_tables(db_path: &Path) -> rusqlite::Result<()> {
     // Create the Users Table
     conn.execute(
         "CREATE TABLE IF NOT EXISTS Users (\
-        UserID INTEGER PRIMARY KEY AUTOINCREMENT,\
         Username TEXT NOT NULL UNIQUE,\
         Password TEXT,\
-        Email TEXT\
         )",
         [],
     )?;
@@ -30,21 +29,21 @@ fn create_database_tables(db_path: &Path) -> rusqlite::Result<()> {
     conn.execute(
         "CREATE TABLE IF NOT EXISTS Exams (\
         ExamID INTEGER PRIMARY KEY AUTOINCREMENT,\
-        Title TEXT,\
+        Title TEXT NOT NULL,\
         Description TEXT,\
-        Duration INTEGER\
         )",
         [],
     )?;
 
-    // Create the Exam Creation Table
+    // Create the Exam Creation Table; DateCreated is YYYY-MM-DD format
     conn.execute(
         "CREATE TABLE IF NOT EXISTS ExamCreation (\
-        ExamID INTEGER PRIMARY KEY,\
-        CreatorUserID INTEGER,\
-        DateCreateD TEXT,\
+        ExamID INTEGER NOT NULL,\
+        CreatorUsername TEXT NOT NULL,\
+        DateCreated TEXT NOT NULL,\
+        PRIMARY KEY (ExamID, CreatorUsername)
         FOREIGN KEY (ExamID) REFERENCES Exams(ExamID),\
-        FOREIGN KEY (CreatorUserID) REFERENCES Users(UserID)\
+        FOREIGN KEY (CreatorUsername) REFERENCES Users(Username)\
         )",
         [],
     )?;
@@ -64,6 +63,118 @@ fn create_database_tables(db_path: &Path) -> rusqlite::Result<()> {
     )?;
 
     Ok(())
+}
+
+fn add_exam() {
+    let db_path: PathBuf = PathBuf::from(format!(
+        "..{}..{}{}",
+        MAIN_SEPARATOR_STR, MAIN_SEPARATOR_STR, ASSET_DIR_NAME
+    ));
+    let conn: Connection = Connection::open(db_path).expect("Unable to access DB for adding exam");
+
+    fn add_question_to_exam(exam: i32) -> bool {
+        let mut result: bool = false;
+        // TODO:
+        return result;
+    }
+
+    fn add_exam_manually() -> bool {
+        let mut result: bool = false;
+        let mut input_string: String = String::new();
+
+        input_string.clear();
+        print!("Enter the ExamID: ");
+        stdout().flush().expect("unable to flush stdout");
+        stdin()
+            .read_line(&mut input_string)
+            .expect("unable to read stdin...");
+
+        if let Ok(exam_id) = input_string.trim().parse::<i32>() {
+            input_string.clear();
+            print!("Enter the exam creator's username: ");
+            stdout().flush().expect("Unable to flush stdout");
+            stdin()
+                .read_line(&mut input_string)
+                .expect("unable to read stdin...");
+
+            if let Ok(creator) = input_string.trim().parse::<String>() {
+                input_string.clear();
+                println!("Is the following entry correct?");
+                println!("\tExam Id:\t{}, Creator:\t{}", exam_id, creator);
+                print!("Is the above correct (Y/n)?:");
+                stdin()
+                    .read_line(&mut input_string)
+                    .expect("Unable to read stdin...");
+            } else {
+                println!("Please enter a valid choice...");
+            }
+        } else {
+            println!("Please enter a valid choice...");
+        }
+        result
+    }
+
+    fn add_exam_by_script() -> bool {
+        let mut result: bool = false;
+        // TODO
+        result
+    }
+
+    fn add_exam_creator(exam_id: i32, creator_username: String, conn: &Connection) -> bool {
+        let mut result: bool = false;
+        let current_date: DateTime<Utc> = chrono::Utc::now();
+        let year = current_date.year();
+        let month = current_date.month();
+        let day = current_date.day();
+
+        if conn
+            .execute(
+                "INSERT INTO\
+            ExamCreation (ExamID, CreatorUsername, DateCreated) \
+            VALUES (?1, ?2, ?3)",
+                [
+                    &exam_id,
+                    &creator_username,
+                    format!("{}-{}-{}", year, month, day).as_str(),
+                ],
+            )
+            .is_err()
+        {
+            println!("Unable to update ExamCreator Table");
+        } else {
+            result = true;
+        }
+
+        result
+    }
+
+    let mut input_string: String = String::new();
+
+    // Use input to see if the user wants to enter a .sql script or manually enter
+    loop {
+        input_string.clear();
+        print!("Enter '1' to enter exam via .sql script or '2' to enter manually: ");
+        stdout().flush().expect("Unable to flush stdout...");
+        stdin()
+            .read_line(&mut input_string)
+            .expect("Unable to parse stdin...");
+
+        let choice = input_string.trim().parse::<i32>();
+        match choice {
+            Ok(num) if num == 1 || num == 2 => {
+                let completed: bool = match num {
+                    1 => add_exam_by_script(),
+                    _ => add_exam_manually(),
+                };
+                if completed {
+                    break;
+                } else {
+                    println!("Please enter a valid choice...");
+                }
+            }
+            _ => println!("Please enter a valid choice..."),
+        }
+    }
 }
 
 fn _execute_sql_from_file(db_path: &Path, sql_file_path: &Path) {
@@ -152,11 +263,10 @@ fn get_and_insert_user(db_path: &Path) -> rusqlite::Result<()> {
 
     let username: String = get_valid_username(&conn);
     let password: String = get_valid_input("Password");
-    let email: String = get_valid_input("Email");
 
     conn.execute(
-        "INSERT INTO Users (Username, Password, Email) VALUES (?1, ?2, ?3)",
-        [&username, &password, &email],
+        "INSERT INTO Users (Username, Password, Email) VALUES (?1, ?2)",
+        [&username, &password],
     )?;
 
     Ok(())
