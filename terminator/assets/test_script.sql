e859363ae2bef85d3fa3a23ab38aac6963502bca
Jake McGrath <jmcgra15@depaul.edu>
2023-11-07 21:12:35 -0600

    Updated to test_script.sql to use newline characters when delineating the options.

--- terminator/assets/test_script.sql
+++ terminator/assets/test_script.sql
@@ -8,4 +8,4 @@ INSERT INTO Exams (Title, Description) VALUES ("EXAM1", "Test Exam...");
 INSERT INTO ExamCreation (ExamID, CreatorUsername, DateCreated) VALUES (1, "Jake", "03NOV23");
 
 INSERT INTO Questions (QuestionText, Options, CorrectAnswer, Explanation, ExamID) VALUES
-    ("Test question prompt:", "A.) 1&B.) 2&C.) 3", "B.) 2", "Because I said so...", 1);
+    ("Test question prompt:", "A.) 1\nB.) 2\nC.) 3", "B.) 2", "Because I said so...", 1);
